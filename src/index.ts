6b6663143ff301f42c3f97dce985bf9bc20dad0d
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2021-06-30 17:31:51 +0900

    feat: create PaizaIO class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-export default function Greet(target: string) {
-  return `Hello, ${target}!`;
+import axios from "axios";
+
+import { BASE_URL } from "./constants";
+import { Language } from "./types";
+import { Runner } from "./lib/runner";
+
+export default class PaizaIO {
+  apiKey: string;
+
+  constructor(args: { apiKey: string }) {
+    this.apiKey = args.apiKey;
+  }
+
+  async createRunner(args: {
+    sourceCode: string;
+    language: Language;
+    input?: string;
+    longpoll?: boolean;
+    lonpollTimeout?: number;
+  }): Promise<Runner> {
+    const url = `${BASE_URL}/runners/create`;
+    const res = await axios.post(url, {
+      api_key: this.apiKey,
+      source_code: args.sourceCode,
+      language: args.language,
+      input: args.input,
+      longpoll: args.longpoll,
+      longpoll_timeout: args.lonpollTimeout
+    });
+
+    return new Runner({id: res.data.id, apiKey: this.apiKey});
+  }
 }
